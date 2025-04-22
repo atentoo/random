8753823d6896cd308f26456f1bea265776c13b40
atentoo <ashley.atento@gobolt.com>
2025-04-22 14:36:52 +0000

    testing

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import App from './App.js'
+import { GenesisProvider } from '@gobolt/genesis'
 
 createRoot(document.getElementById('root')).render(
+  <GenesisProvider>
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
+  </GenesisProvider>
 )
