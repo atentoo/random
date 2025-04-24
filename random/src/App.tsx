a63c328ed2a0d7b153d5165e3a3d46b2fe902692
atentoo <ashley.atento@gobolt.com>
2025-04-24 21:28:54 +0000

    added cat button functions

--- random/src/App.tsx
+++ random/src/App.tsx
@@ -1,25 +1,36 @@
-
-import { Card, Button, THEME, GENESIS } from '@gobolt/genesis'
-import { SettingOutlined, EditOutlined, EllipsisOutlined } from '@ant-design/icons'
-
+import { useState } from 'react'
+import { Card, Button, Typography } from '@gobolt/genesis'
 
 function App() {
-  
+  const [pic, setPic] = useState("Select a Button");
+  const handleClick = (newPic: any) => {
+    setPic(newPic)
+  };
   return (
     <>
       <Card
-    style={{ width: 500}}
-    actions={[
-        <Button themeType="primary">funny quote</Button>,
-        <Button themeType="secondary">sad quote</Button>,
-        <Button themeType="tertiary">meme</Button>,
-    ]}
-    >
-      <div>
-          A slot to add any component or provide text like a product description
-      </div>
+        style={{
+          width: 500,
+          margin: '0 auto',
+          textAlign: 'center',
+        }}
+
+        actions={[
+          <Button themeType="primary" onClick={() => handleClick(<img src="https://media.tenor.com/CJAzSgd6Vu8AAAAe/cute-cat-cat.png" />)}>happy cat</Button>,
+          <Button themeType="secondary" onClick={() => handleClick(<img src="https://cdn-useast1.kapwing.com/static/templates/crying-cat-meme-template-full-719a53dc.webp" style={{maxHeight: '200px'}} />)}>sad cat</Button>,
+          <Button themeType="tertiary" onClick={() => handleClick(<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTdBWB76EZKUgHdARYa-XNyIzoiJiUiyKiFrg&s" />)}>?</Button>,
+        ]}
+      >
+        <div>
+        <Typography
+          themeType='secondary'
+          variant='display1'
+          style={{ textAlign: 'center', paddingBottom: '20px' }}
+            >Pick a cat</Typography>
+          <Typography variant="heading3">{pic}</Typography>
+        </div>
       </Card>
-  </>
+    </>
   )
 }
 
